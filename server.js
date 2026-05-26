1b1a4d8f4faefe4c064b4d861297ea1f590c6cbf
mb33377 <mb33377@gmail.com>
2026-05-26 18:04:39 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Renobach Bexio MCP Server Running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
